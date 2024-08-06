37f3e9316e95ea2c54bcdf89df35f0690555700e
yuriy05 <dilayyura@gmail.com>
2024-08-06 20:41:16 +0300

    Fast React Pizza Co. �

--- src/index.js
+++ src/index.js
@@ -1,17 +1,87 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { pizzaData } from './data';
+
+function App() {
+    return (
+        <div className="container">
+            <Header />
+            <Menu />
+            <Footer />
+        </div>
+    );
+}
+
+function Header() {
+    console.log(pizzaData);
+
+    return (
+        <header className="header">
+            <h1>Fast React Pizza Co.</h1>
+        </header>
+    );
+}
+
+function Menu() {
+    const pizzas = pizzaData;
+    const numPizzas = pizzaData.length;
+    return (
+        <div className="menu">
+            <h2>Our Menu</h2>
+            {numPizzas > 0 ? (
+                <ul className="pizzas">
+                    {pizzas.map((pizza) => (
+                        <Pizza pizzaObj={pizza} key={pizza.name} />
+                    ))}
+                </ul>
+            ) : null}
+
+            <button className="btn">Order</button>
+            {/* <Pizza 
+                name="Pizza Spinaci"
+                ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+                photoName="images/pizzas/spinaci.jpg"
+                price={10} 
+            />
+            <Pizza
+                name="Pizza Funghi"
+                ingredients="Tomato, mozarella, mushrooms, and onion"
+                photoName="images/pizzas/funghi.jpg"
+                price={10} 
+            /> */}
+        </div>
+    );
+}
+
+function Pizza({ pizzaObj }) {
+    return (
+        <li className={`pizza ${pizzaObj.soldOut === true ? `sold-out` : ''}`}>
+            <img src={`images/` + pizzaObj.photoName} alt={pizzaObj.name} />
+            <div>
+                <h3>{pizzaObj.name}</h3>
+                <p>{pizzaObj.ingredients}</p>
+                <span>{pizzaObj.price}</span>
+            </div>
+        </li>
+    );
+}
+
+function Footer() {
+    const hour = new Date().getHours();
+    const openHour = 9;
+    const closedHour = 21;
+    const isOpen = hour >= openHour && hour <= closedHour;
+    return (
+        <footer className="footer">
+            {isOpen ? 'We are currently open !' : 'We are closed !'}
+        </footer>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
